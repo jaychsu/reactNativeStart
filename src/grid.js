c2d256de6b7304e80dac7bd6529c556b3e369071
Jaych Su <jaych.su@autodesk.com>
2016-11-01 17:17:18 +0800

    Finished grid view

--- src/grid.js
+++ src/grid.js
@@ -1,23 +1,65 @@
 import React, { Component } from 'react'
 import {
   Text,
-  View
+  Image,
+  View,
+  ListView,
+  TouchableHighlight
 } from 'react-native'
 import style from './style'
 
+const THUMB_URLS = []
+
 export default class Grid extends Component {
+  constructor() {
+    super()
+
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+    this.state = {
+      dataSource: ds.cloneWithRows(THUMB_URLS)
+    }
+  }
+
   render() {
     return (
-      <View style={ this.props.style }>
-        <View style={{
+      <ListView
+        dataSource={ this.state.dataSource }
+        renderRow={ this._renderRow }
+
+        initialListSize={ 21 }
+        pageSize={ 5 }
+        contentContainerStyle={{
+          justifyContent: 'space-around',
           flexDirection: 'row',
-          flex: 1,
+          flexWrap: 'wrap',
+          alignItems: 'flex-start',
+
+          paddingLeft: 20,
+          paddingRight: 20
+        }}
+      />
+    )
+  }
+
+  _renderRow(rowData: string, sectionID: number, rowID: number) {
+    let imgSrc = THUMB_URLS[rowID]
+    return (
+      <TouchableHighlight underlayColor="transparent">
+        <View style={{
           justifyContent: 'center',
-          alignItems: 'center'
+          alignItems: 'center',
+          paddingLeft: 10,
+          paddingRight: 10,
+          paddingTop: 10,
+          paddingBottom: 4
         }}>
-          <Text style={ style.content }>SketchBook Online Gallery</Text>
+          <Image style={{ width: 64, height: 64 }} source={{ uri: imgSrc }} />
+          <Text style={[
+            style.content,
+            { paddingTop: 4 }
+          ]}>{ rowID }</Text>
         </View>
-      </View>
+      </TouchableHighlight>
     )
   }
 }

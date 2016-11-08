1eb36dbe5e75596da1021b5e9f413a8aab6b4bfe
Jaych Su <jaych.su@autodesk.com>
2016-11-08 16:50:27 +0800

    Abstract config from `img-source`

--- src/asset/img-source.js
+++ src/asset/img-source.js
@@ -1,7 +1,12 @@
 import HttpImgSource from './http-img'
 import LocalImgSource from './local-img'
 
-const BaseSource = HttpImgSource
+const config = {
+  source: HttpImgSource,
+  thumbsLength: 50
+}
+
+const BaseSource = config.source
 
 class ImgSource extends BaseSource {
   constructor() {
@@ -9,7 +14,7 @@ class ImgSource extends BaseSource {
 
     this.getThumbSrc = this.getThumbSrc.bind(this)
 
-    this.thumbsLength = 200
+    this.thumbsLength = config.thumbsLength
     this.thumbs = []
 
     for (let i = 0; i < this.thumbsLength; i++) {

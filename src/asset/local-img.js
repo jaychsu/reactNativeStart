a59fdbe18e455f919f3934e768f47f74b34be753
Jaych Su <jaych.su@autodesk.com>
2016-11-10 10:36:25 +0800

    Save local img resource into repo to avoid error when compiling if in new env

--- src/asset/local-img.js
+++ src/asset/local-img.js
@@ -3,10 +3,9 @@ export default class LocalImgSource {
     this.getUri = this.getUri.bind(this)
 
     this.srcs = [
-      require('./distort_transform-2d19945d3fad0143983904c9c990e35f.jpg'),
-      require('./flipbook_animation-35934b3cf75a4406e816dc297ec0d1e2.jpg'),
-      require('./perspective_guides-2cac1ddd833a315a2a091f20388766a1.jpg'),
-      require('./full-color.jpg')
+      require('./distort_transform.jpg'),
+      require('./flipbook_animation.jpg'),
+      require('./perspective_guides.jpg')
     ]
   }
 

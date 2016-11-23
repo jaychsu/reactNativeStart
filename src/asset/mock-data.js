1b337c6842dbc45c3bdfa73ab4c9aa58bc8a65cd
Jaych Su <jaych.su@autodesk.com>
2016-11-23 11:27:49 +0800

    Implement `MockData` to get mock data

---
+++ src/asset/mock-data.js
@@ -0,0 +1,30 @@
+import HttpImgSource from './http-img'
+import LocalImgSource from './local-img'
+
+import { arrItemMove } from '../util'
+
+const BaseSource = LocalImgSource
+
+export default class MockData extends BaseSource {
+  constructor(dataLength) {
+    super()
+
+    this.data = {}
+    this.sorter = []
+    this.dataLength = dataLength
+
+    for (let i = 0; i < this.dataLength; i++) {
+      let randomImgIndex = Math.floor(Math.random() * this.srcs.length)
+      this.data[i] = {
+        thumbnail: this.srcs[randomImgIndex],
+        title: `Image ${i}`
+      }
+      this.sorter.push(i)
+    }
+  }
+
+  move(from, to) {
+    arrItemMove(this.sorter, from, to)
+    return this.sorter
+  }
+}
